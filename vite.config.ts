0ab929097528a3c1eae0e5540bd75248453c8287
farsousa <andersoncaj26@gmail.com>
2023-05-31 00:41:23 -0300

    ajustanco importação css material icons

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['material-design-icons/iconfont/material-icons.css']
+    }
   }
 })
